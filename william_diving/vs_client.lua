aa9b2879d132585ba883428b5ec3284aab9a98b6
William B17 <76631826+William-B17@users.noreply.github.com>
2022-07-12 01:21:55 +0200

    Update vs_client.lua

--- william_diving/vs_client.lua
+++ william_diving/vs_client.lua
@@ -1,26 +1,44 @@
+
 ScubaToggle = true
 
 RegisterCommand('w_div', function(source)
-	local Oxygen = 100.0
+	local Oxygen = 11.0
 	local ped = GetPlayerPed(-1)
 	local OxygenTank = Oxygen
+
+	function removeRebreather()
+		SetPedComponentVariation(ped, 1, 0, 0, 0)
+		SetPedComponentVariation(ped, 8, 0, 0, 0)
+		SetEnableScuba(ped, false)
+		SetEnableScubaGearLight(ped, false)
+	end
+
+	function giveRebreather()
+		SetPedComponentVariation(ped, 1, 36, 0, 0)
+		SetPedComponentVariation(ped, 8, 123, 0, 0)
+		SetEnableScuba(ped, true)
+		SetEnableScubaGearLight(ped, true)
+	end
 
 	function checkUnderWater()
 		while IsPedSwimmingUnderWater(ped) do
 			Citizen.Wait(1000)
-			if(Oxygen > 0) then
+			if(Oxygen > 10) then
+				giveRebreather()
+				Oxygen = Oxygen - 1
+			elseif(Oxygen > 0) then
 				Oxygen = Oxygen - 1
 				print(Oxygen)
-				SetEnableScuba(ped, true)
-				SetEnableScubaGearLight(ped, true)
 				SetPedMaxTimeUnderwater(ped, OxygenTank)
 			elseif(Oxygen <= 0) then
-				print("Du burde være død")
+				print("Hej hej luft")
 				ScubaToggle = false
+				removeRebreather()
 				break
 			end
 		end
 	end
+
 
 	while ScubaToggle do
 		Citizen.Wait(0)
